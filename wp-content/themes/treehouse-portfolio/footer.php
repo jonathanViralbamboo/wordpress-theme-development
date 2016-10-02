f24721a088d45cf2b86549cf503baa7082944723
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-10-02 15:46:13 +0100

    Porting Existing Headers & Footers
    porting over the static code from the original index.html file, add it
    to the header and footer files, and then begin replacing static content
    with dynamic WordPress code.

--- wp-content/themes/treehouse-portfolio/footer.php
+++ wp-content/themes/treehouse-portfolio/footer.php
@@ -1,1 +1,9 @@
-<p>Footer</p>
+        <div class="footer-clear"></div>
+        <footer class="row no-max pad">
+            <p>Copyright <?php echo date('Y'); ?></p>
+        </footer>
+
+        <?php wp_footer(); ?>
+        
+    </body>
+</html>
