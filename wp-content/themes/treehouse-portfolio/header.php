459e1bb6fd1d61a7616fff96c710d99fc66c0c08
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-10-06 17:28:58 +0100

    Portfolio & Nav
    - Creating dynamic navigation
    - Custom post type templates
    - Portfolio home page

--- wp-content/themes/treehouse-portfolio/header.php
+++ wp-content/themes/treehouse-portfolio/header.php
@@ -6,16 +6,26 @@
         <title><?php wp_title(); ?></title>
 
         <?php wp_head(); ?>
-        
+
     </head>
 
-  <body>
-    <header class="row no-max pad main">
+  <body <?php body_class(); ?>>
+<header class="row no-max pad main">
   <h1><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
   <a href="" class="nav-toggle"><span></span>Menu</a>
   <nav>
     <h1 class="open"><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-    <ul class="no-bullet">
+
+    <?php
+    $defaults = array(
+      'container' => false,
+      'menu' => 'primary-menu',
+      'menu_class' => 'no-bullet'
+    );
+    wp_nav_menu($defaults);
+    ?>
+
+    <!-- <ul class="no-bullet">
       <li class="current parent"><a class='current' href="index.html">Portfolio</a>
         <ul class="sub-menu">
           <li><a href="item.html">Portfolio Item</a></li>
@@ -32,6 +42,6 @@
       </li>
       <li><a href="about.html">About</a></li>
       <li><a href="contact.html">Contact</a></li>
-    </ul>
+    </ul> -->
   </nav>
 </header>
--- wp-content/themes/treehouse-portfolio/page.php
+++ wp-content/themes/treehouse-portfolio/page.php
@@ -10,7 +10,7 @@
       <p><?php the_content(); ?></p>
 
     <?php endwhile; else : ?>
-      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <p><?php _e( 'Sorry, no pages found.' ); ?></p>
     <?php endif; ?>
 
     </div>
